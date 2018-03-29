693632654901afb4ed0bfc4fb4b5edaf40381950
John McPherson <jcmcph@gmail.com>
2018-03-29 00:02:55 -0500

    update package

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     name='django-remote-media',
     version='0.2',
     url='https://github.com/jcmcp/django-remote-media',
+    download_url='https://pypi.python.org/pypi/django-remote-media',
     license='MIT',
     author='John McPherson',
     author_email='jcmcph@gmail.com',
@@ -18,5 +19,11 @@ setup(
     install_requires=[
         'requests',
         'Django',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License'
     ]
 )
